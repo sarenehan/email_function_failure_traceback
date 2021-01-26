ff8863c523d3f836f862baede5d5983d6b6d8d95
Stewart Renehan <stewart@pellego.com>
2021-01-26 15:19:50 -0800

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="email_function_failure_traceback",
-    version="0.0.1",
+    version="0.0.2",
     author="Stewart Renehan",
     author_email="sarenehan@gmail.com",
     description="Failed Function Error Traceback Email.",
